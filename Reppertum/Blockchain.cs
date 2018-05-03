8770418450f9645df9e3cef333ce5b57e254abfc
Dennis-Furrer <dennis.furrer@ibm.com>
2018-05-03 18:25:57 +0100

    Added multi-transaction block functionality.

--- Reppertum/Blockchain.cs
+++ Reppertum/Blockchain.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace Reppertum
-{
-    public class Blockchain
-    {
+namespace Reppertum {
+    public class Blockchain {
         public readonly List<Block> Chain = new List<Block>();
         public readonly string FirstHash;
         
         private UInt16 _current = 0;
 
-        public Blockchain(string prevHash, string data, Int64 timestamp) {
+        public Blockchain(string prevHash, List<Transaction> data, Int64 timestamp) {
             string hash = GetHash(_current, prevHash, data, timestamp);
             FirstHash = hash;
             Block b = new Block(_current, prevHash, hash, data, timestamp);
@@ -20,7 +18,7 @@ namespace Reppertum
             _current++;
         }
 
-        public Block AddBlock(string prevHash, string data, Int64 timestamp) {
+        public Block AddBlock(string prevHash, List<Transaction> data, Int64 timestamp) {
             Block prevB = Chain[_current-1];
             string hash = GetHash(_current, prevHash, data, timestamp);
             Block newB = new Block(_current, prevHash, hash, data, timestamp);
@@ -38,39 +36,34 @@ namespace Reppertum
 
             return newB;
         }
+        
+        public Transaction AddTransaction(UInt16 index, string from, string to, string data) {
+            string hashable = index + from + to + data;
+            return new Transaction(index, Cryptography.Sha256(hashable), from, to, data, DateTime.UtcNow.Ticks);
+        }
 
         public Block GetBlock(UInt16 index) {
             return Chain[index];
         }
 
-        public string GetHash(UInt16 i, string prevHash, string data, Int64 timestamp) {
-            return Hash(i.ToString() + prevHash + data + timestamp);
+        public string GetHash(UInt16 i, string prevHash, List<Transaction> data, Int64 timestamp) {
+            return Cryptography.Sha256(i.ToString() + prevHash + data + timestamp);
         }
 
         public Int32 GetNumberOfBlocks() {
             return Chain.Count;
         }
 
-        private static string Hash(string str) {
-            SHA256Managed crypt = new SHA256Managed();
-            StringBuilder hash = new StringBuilder();
-            Byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(str));
-            foreach (Byte theByte in crypto)
-            {
-                hash.Append(theByte.ToString("x2"));
-            }
-            return hash.ToString();
-        }
-
-        private static bool ProofOfWork(Block prevB, Block newB, UInt16 difficulty = 5)
+        private bool ProofOfWork(Block prevB, Block newB, UInt16 difficulty = 5)
         {
+            Console.Clear();
             Console.WriteLine("Computing Proof-of-Work...");
             bool valid = false;
             UInt32 nonce = 0;
             string header = newB.Index + newB.PreviousHash;
             string hash;
             do {
-                hash = Hash(header + nonce);
+                hash = Cryptography.Sha256(header + nonce);
                 nonce++;
             } while (hash.Substring(0, difficulty - 1) != "0000");
             
---
+++ Reppertum/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Reppertum {
+    public class Transaction {
+        public readonly UInt16 Index;
+        public readonly string Hash, FromAddress, ToAddress, Data;
+        public readonly Int64 Timestamp;
+
+        public Transaction(UInt16 index, string hash, string fromAddress, string toAddress, string data, Int64 timestamp) {
+            Index = index;
+            Hash = hash;
+            FromAddress = fromAddress;
+            ToAddress = toAddress;
+            Data = data;
+            Timestamp = timestamp;
+        }
+    }
+}
