b2b6064a3f5abdc13e4df3a8e3fa69c280b6b058
Dennis-Furrer <dennis.furrer@ibm.com>
2018-05-04 10:32:01 +0100

    Removed hardcoded difficulty setting for PoW.

--- Reppertum/Blockchain.cs
+++ Reppertum/Blockchain.cs
@@ -73,15 +73,20 @@ namespace Reppertum
             UInt32 nonce = 0;
             string header = newB.Header.Index + newB.Header.PreviousHash;
             string hash;
+            string diff = string.Empty;
+            for (int i = 0; i < difficulty-1; i++)
+            {
+                diff += "0";
+            }
             
             do 
             {
                 hash = Cryptography.Sha256(header + nonce);
                 nonce++;
             } 
-            while (hash.Substring(0, difficulty - 1) != "0000");
+            while (hash.Substring(0, difficulty - 1) != diff);
             
-            valid = (hash.Substring(0, difficulty-1) == "0000");
+            valid = (hash.Substring(0, difficulty-1) == diff);
             
             Console.WriteLine("Successfully computed!");
             
