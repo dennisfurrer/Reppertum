9abbf073b81ce06d9c127fd71c0e7e572c02ee5f
Ethiril <xaiosystemburn@gmail.com>
2018-05-07 00:57:43 +0100

    Transaction interface

---
+++ Reppertum/Core/ITransaction.cs
@@ -0,0 +1,12 @@
+﻿namespace Reppertum.Core
+{
+    public interface ITransaction
+    {
+        string data { get; set; }
+        string fromAddress { get; set; }
+        string hash { get; set; }
+        ushort index { get; set; }
+        long timestamp { get; set; }
+        string toAddress { get; set; }
+    }
+}
